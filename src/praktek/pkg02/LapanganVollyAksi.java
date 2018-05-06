8f839f5e2a549cb7b433f7fbe7146ce451100c69
Rudi Noor Evendi <rudievendi69@yahoo.com>
2018-05-06 18:40:07 +0800

    Mengisi Nilai Atribut

--- src/praktek/pkg02/LapanganVollyAksi.java
+++ src/praktek/pkg02/LapanganVollyAksi.java
@@ -3,6 +3,8 @@ package praktek.pkg02;
 public class LapanganVollyAksi {
     public static void main(String[] args) {
         LapanganVolly L1 = new LapanganVolly();
+        L1.panjang = 18;
+        L1.lebar = 9;
     }
     
 }
