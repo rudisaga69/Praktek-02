0ce24d95800a043e3e4d86c24a8dd3d0c6051bca
Rudi Noor Evendi <rudievendi69@yahoo.com>
2018-05-06 18:28:12 +0800

    membuat attribut dan behavior

--- src/praktek/pkg02/LapanganVolly.java
+++ src/praktek/pkg02/LapanganVolly.java
@@ -1,5 +1,14 @@
 package praktek.pkg02;
 
 public class LapanganVolly {
+    double panjang;
+    double lebar;
+    
+    void cetakInfo(){
+        System.out.println("==================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar : "+lebar);
+        System.out.println("==================");
+    }
     
 }
